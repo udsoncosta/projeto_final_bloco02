9289b4e2dc4efed13e43106459858c375df19dbf
udsoncosta <udsoncostasantana@gmail.com>
2023-10-10 15:36:27 -0300

    Configurando projeto na nova branch

---
+++ EcommerceFarmacia/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceFarmacia.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- EcommerceFarmacia/Program.cs
+++ EcommerceFarmacia/Program.cs
@@ -1,3 +1,8 @@
+
+using EcommerceFarmacia.Data;
+using Microsoft.EntityFrameworkCore;
+using projeto_farmacia.Data;
+
 namespace EcommerceFarmacia
 {
     public class Program
@@ -10,9 +15,48 @@ namespace EcommerceFarmacia
 
             builder.Services.AddControllers();
 
+            // Conexão com o Banco de dados
+            var connectionString = builder.Configuration.
+                    GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Configuração do CORS
+            builder.Services.AddCors(options => {
+                options.AddPolicy(name: "MyPolicy",
+                    policy =>
+                    {
+                        policy.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                    });
+            });
+
             var app = builder.Build();
 
+            // Criar o Banco de dados e as tabelas Automaticamente
+            using (var scope = app.Services.CreateAsyncScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                dbContext.Database.EnsureCreated();
+            }
+
+            app.UseDeveloperExceptionPage();
+
             // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("MyPolicy");
 
             app.UseAuthorization();
 
